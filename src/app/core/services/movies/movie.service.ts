54891688301ac0f961e4adf57d065f8b3e8b653d
tarun0582 <tarun.sharma@chicmic.co.in>
2023-05-01 18:28:28 +0530

    service

--- src/app/core/services/movies/movie.service.ts
+++ src/app/core/services/movies/movie.service.ts
@@ -1,15 +1,21 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
   private http: HttpClient = inject(HttpClient);
+  params:any
+  params2:any
+  details:any={
+    page:'1'
+  }
   constructor() { }
   getMovies(){
-   
-      return this.http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=7064b20a5bfe960aec0dd3ceaaaf82c9&page=1&language=en-US&region=US');
+    this.params = new HttpParams()
+    .set('api_key', '7064b20a5bfe960aec0dd3ceaaaf82c9').append('page', '2')
+      return this.http.get('https://api.themoviedb.org/3/movie/now_playing',{params:this.params});
     
   }
 }
